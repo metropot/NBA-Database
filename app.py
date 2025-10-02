c88d60bcb861a5096f1fc65e8bb078ece054b1fe
metropot <metropot@gmail.com>
2025-10-01 19:39:38 -0700

    Initial commit - NBA Database API

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ import os
 
 app.config["SQLALCHEMY_DATABASE_URI"] = (
     f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
-))
+)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
